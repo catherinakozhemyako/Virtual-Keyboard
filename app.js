2b026a22519216f7b1a46ef224b4923388b83113
catherinakozhemyako <catherine.kozhemyako@gmail.com>
2023-05-01 17:47:30 +0300

    refactor: change styles

--- app.js
+++ app.js
@@ -1,17 +1,39 @@
 let lang = 'ru';
-let ruKeys = [['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'], ['Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', 'Delete'],
-  ['CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter'], ['Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', 'Up', 'Shift'],
-  ['Ctrl', 'Win', 'Alt', ' ', 'Alt', 'Left', 'Down', 'Right', 'Ctrl']];
+let capsMode = false;
+let shift = false;
 
-function createHeader() {
-  const header = document.createElement('header');
-  header.classList.add('header');
-  document.body.appendChild(header);
-  const title = document.createElement('h1');
-  title.classList.add('title');
-  title.innerText = 'Virtual Keyboard';
-  header.appendChild(title);
-}
+const ruKeys = [['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'], ['Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '/', 'Del'],
+  ['CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter'], ['Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', 'Up', 'Shift'],
+  ['Ctrl', 'Win', 'Alt', 'Space', 'Alt', 'Left', 'Down', 'Right', 'Ctrl']];
+
+const ruKeysShift = [['Ё', '!', '"', '№', ';', '%', ':', '?', '*', '(', ')', '_', '+', 'Backspace'], ['Tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', '\\', 'Del'],
+  ['CapsLock', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э', 'Enter'], ['Shift', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', '.', 'Up', 'Shift'],
+  ['Ctrl', 'Win', 'Alt', 'Space', 'Alt', 'Left', 'Down', 'Right', 'Ctrl']];
+
+const enKeys = [['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'], ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Del'],
+  ['CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'Enter'], ['Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'Up', 'Shift'],
+  ['Ctrl', 'Win', 'Alt', 'Space', 'Alt', 'Left', 'Down', 'Right', 'Ctrl']];
+
+const enKeysShift = [['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace'], ['Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '|', 'Del'],
+  ['CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', 'Enter'], ['Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', 'Up', 'Shift'],
+  ['Ctrl', 'Win', 'Alt', 'Space', 'Alt', 'Left', 'Down', 'Right', 'Ctrl']];
+
+  function createHeader() {
+    const header = document.createElement('header');
+    header.classList.add('header');
+    document.body.appendChild(header);
+    const title = document.createElement('h1');
+    title.classList.add('title');
+    title.innerText = 'Virtual Keyboard';
+    header.appendChild(title);
+  }
+  function createFooter() {
+    const footer = document.createElement('footer');
+    footer.innerHTML = '<p>Для переключения языка комбинация: левыe ctrl + alt</p>';
+    footer.classList.add('footer');
+    document.body.appendChild(footer);
+  }
+
 function createTextArea() {
   const textArea = document.createElement('textarea');
   textArea.classList.add('textarea');
@@ -19,40 +41,78 @@ function createTextArea() {
 }
 
 function createButton(role) {
-  const button = document.createElement('div');
-  button.classList.add('key');
-  button.innerHTML = `<span>${role}</span>`;
-  return button;
-}
+    const button = document.createElement('div');
+    button.classList.add('key');
+    button.classList.add(`key-${role}`);
+    if (capsMode === true) {
+      button.innerHTML = `<span>${role.toUpperCase()}</span>`;
+    }
+    button.innerHTML = `<span>${role}</span>`;
+    return button;
+  }
 
 function createRow(arr) {
   const row = document.createElement('div');
   row.classList.add('keyboard-row');
-  arr.forEach(element => {
+  arr.forEach((element) => {
     const button = createButton(element);
     row.appendChild(button);
   });
   return row;
 }
 
-function createKeyboard(lang) {
+function createKeyboard(lang, shift) {
   const keyboardWrapper = document.createElement('div');
   keyboardWrapper.classList.add('keyboard-wrapper');
   document.body.appendChild(keyboardWrapper);
-  if (lang === 'ru') {
+  if (lang === 'ru' && shift === false) {
     const keyboard = document.createElement('div');
-    ruKeys.forEach(element => {
-      let row = createRow(element);
+    ruKeys.forEach((element) => {
+      const row = createRow(element);
+      keyboard.appendChild(row);
+    });
+    keyboardWrapper.appendChild(keyboard);
+  }
+  if (lang === 'ru' && shift === true) {
+    const keyboard = document.createElement('div');
+    ruKeysShift.forEach((element) => {
+      const row = createRow(element);
+      keyboard.appendChild(row);
+    });
+    keyboardWrapper.appendChild(keyboard);
+  }
+  if (lang === 'en' && shift === false) {
+    const keyboard = document.createElement('div');
+    enKeys.forEach((element) => {
+      const row = createRow(element);
+      keyboard.appendChild(row);
+    });
+    keyboardWrapper.appendChild(keyboard);
+  }
+  if (lang === 'en' && shift === true) {
+    const keyboard = document.createElement('div');
+    enKeysShift.forEach((element) => {
+      const row = createRow(element);
       keyboard.appendChild(row);
     });
     keyboardWrapper.appendChild(keyboard);
   }
   return keyboardWrapper;
 }
-
 window.addEventListener('load', () => {
   createHeader();
   createTextArea();
-  createKeyboard(lang);
+  createKeyboard(lang, shift);
+  createFooter();
+
+  for (let elementKey of document.getElementsByClassName('key')) {
+    elementKey.addEventListener('mousedown', () => {
+      keyDown(elementKey);
+    });
+    const mouseUpListener = () => {
+      keyUp(elementKey);
+    };
+    elementKey.addEventListener('mouseup', mouseUpListener)
+    elementKey.addEventListener('mouseleave', mouseUpListener)
   }
-);
+});
